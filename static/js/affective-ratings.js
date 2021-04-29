e0145aa38507c331e715b3a4554f24ec6cbbe50d
dvdmrn <dgm.marino@gmail.com>
2021-04-28 23:09:23 -0400

    centered scales

--- static/js/affective-ratings.js
+++ static/js/affective-ratings.js
@@ -85,7 +85,7 @@ let getResponses = () => {
 let resetSliders = () => {
     let sliders = document.getElementsByClassName("slider");
     for (let i = 0; i < sliders.length; i++) {
-        sliders[i].value = 3;
+        sliders[i].value = 4;
     }
 }
 
